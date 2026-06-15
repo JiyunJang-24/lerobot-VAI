8033609ca2b0bb0d03cd4caa97eba4518cf33923
JiyunJang-24 <fick17@korea.ac.kr>
2026-06-15 11:18:51 +0000

    add train smolvla with plucker

--- train_smolVLA.sh
+++ train_smolVLA.sh
@@ -119,6 +119,64 @@ CUDA_VISIBLE_DEVICES=0 python src/lerobot/scripts/lerobot_train.py \
 # Training checkpoints will be saved under: lerobot/outputs/train/202x-xx-xx/xx-xx-xx_diffusion
 # --wandb.project=smolVLA_wrist_libero_goal \
 
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"
+export PYTHONPATH="${SCRIPT_DIR}/src:${SCRIPT_DIR}/third_party:${SCRIPT_DIR}/third_party/LIBERO/libero:${PYTHONPATH:-}"
+
+CUDA_VISIBLE_DEVICES=0 python src/lerobot/scripts/lerobot_train.py \
+  --dataset.repo_id=[libero_10/RMA_vla_50_01_330.0_330.0/v-1.000-1.000_num2,libero_goal/RMA_vla_50_01_15.0_15.0/v-1.000-1.000_num1,libero_object/RMA_vla_50_01_30.0_30.0/v-1.000-1.000_num1,libero_spatial/RMA_vla_50_01_0.0_0.0/v-1.000-1.000_num1,libero_10/RMA_vla_50_01_345.0_345.0/v-1.000-1.000_num3] \
+  --dataset.root="/root/Desktop/workspace/lerobot-VAI/dataset_git/RMA_ex02" \
+  --dataset.cache_in_memory=true \
+  --dataset.use_wrist_cam=false \
+  --dataset.use_state=false \
+  --policy.type="smolvla" \
+  --policy.push_to_hub=false \
+  --steps=50000 \
+  --save_freq=5000 \
+  --batch_size=64 \
+  --wandb.enable=true \
+  --wandb.project="RMA02_libero_smolvla" \
+  --wandb.disable_artifact=true \
+  --wandb.entity="DynamicVLA" \
+  --num_workers=64 \
+  --dataloader_prefetch_factor=8 \
+  --dataloader_persistent_workers=true \
+  --job_name="smolvla_kyc_rma_02_ex2_plucker_encoder_wo_state" \
+  --policy.visual_cue_mode="plucker_concat" \
+  --policy.load_vlm_weights=true \
+  --policy.freeze_vision_encoder=false \
+  --policy.train_expert_only=false
+# Training checkpoints will be saved under: lerobot/outputs/train/202x-xx-xx/xx-xx-xx_diffusion
+# --wandb.project=smolVLA_wrist_libero_goal \
+
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"
+export PYTHONPATH="${SCRIPT_DIR}/src:${SCRIPT_DIR}/third_party:${SCRIPT_DIR}/third_party/LIBERO/libero:${PYTHONPATH:-}"
+
+CUDA_VISIBLE_DEVICES=0 python src/lerobot/scripts/lerobot_train.py \
+  --dataset.repo_id=[libero_10/RMA_vla_50_01_330.0_330.0/v-1.000-1.000_num2,libero_goal/RMA_vla_50_01_15.0_15.0/v-1.000-1.000_num1,libero_object/RMA_vla_50_01_30.0_30.0/v-1.000-1.000_num1,libero_spatial/RMA_vla_50_01_0.0_0.0/v-1.000-1.000_num1,libero_10/RMA_vla_50_01_345.0_345.0/v-1.000-1.000_num3] \
+  --dataset.root="/root/Desktop/workspace/lerobot-VAI/dataset_git/RMA_ex02" \
+  --dataset.cache_in_memory=true \
+  --dataset.use_wrist_cam=false \
+  --dataset.use_state=false \
+  --policy.type="smolvla" \
+  --policy.push_to_hub=false \
+  --steps=50000 \
+  --save_freq=5000 \
+  --batch_size=64 \
+  --wandb.enable=true \
+  --wandb.project="RMA02_libero_smolvla" \
+  --wandb.disable_artifact=true \
+  --wandb.entity="DynamicVLA" \
+  --num_workers=64 \
+  --dataloader_prefetch_factor=8 \
+  --dataloader_persistent_workers=true \
+  --job_name="smolvla_kyc_rma_02_ex2_plucker_wo_state" \
+  --policy.visual_cue_mode="plucker" \
+  --policy.load_vlm_weights=true \
+  --policy.freeze_vision_encoder=false \
+  --policy.train_expert_only=false
+# Training checkpoints will be saved under: lerobot/outputs/train/202x-xx-xx/xx-xx-xx_diffusion
+# --wandb.project=smolVLA_wrist_libero_goal \
 
 # SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"
 # export PYTHONPATH="${SCRIPT_DIR}/src:${SCRIPT_DIR}/third_party:${SCRIPT_DIR}/third_party/LIBERO/libero:${PYTHONPATH:-}"
